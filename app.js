e19ccb027a9967634d78788827851c7ea3ebb41b
TatianaJS <jaya.shanti02@gmail.com>
2024-08-24 12:03:12 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var dropdownElementList = [].slice.call(
+    document.querySelectorAll(".dropdown-toggle")
+)
+
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+    return new bootstrap.Dropdown(dropdownToggleEl)
+})
